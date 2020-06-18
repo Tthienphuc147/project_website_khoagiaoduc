a1ac793af0c202ca1f7757518872e499fd91e906
Tthienphuc147 <43287192+Tthienphuc147@users.noreply.github.com>
2020-06-19 00:00:30 +0700

    --update

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -6,7 +6,7 @@
         <title>Khoa Giáo Dục - Mầm Non</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" type="image/x-icon" href="/public/user/img/logo/favicon.png">
+        <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @include('user.layout.css')
         <script src="{{ asset('/public/user/js/vendor/jquery-1.12.4.min.js') }}"></script>
